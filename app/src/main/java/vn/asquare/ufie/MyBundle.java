236a197bfa8f553c256022b28cdb7fb93cd989e8
ngochung1243 <ngochung1243@gmail.com>
2016-04-14 16:20:12 +0700

    Make bundle

---
+++ app/src/main/java/vn/asquare/ufie/MyBundle.java
@@ -0,0 +1,12 @@
+package vn.asquare.ufie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hungmai on 14/04/2016.
+ */
+public class MyBundle {
+    public static List<String> image_urls = new ArrayList<String>();
+    public static WifiP2PBroadcast mBroadcast;
+}
